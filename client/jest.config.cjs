23d417ce3296b7b375827d7b654fa6822ea93073
SANJU <sanjubodra1420@gmail.com>
2024-02-02 21:51:49 +0530

    test sanju

---
+++ client/jest.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  moduleFileExtensions: ["js", "json", "jsx"],
+  setupFiles: ["<rootDir>/test-config/enzyme.config.cjs"],
+  setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
+  verbose: true,
+};
--- client/src/pages/__tests__/Home.test.js
+++ client/src/pages/__tests__/Home.test.js
@@ -1,10 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import Home from '../Home';
-import { describe, it, expect } from 'vitest';
+import Enzyme from "enzyme";
+import Adapter from "@cfaester/enzyme-adapter-react-18";
 
-describe('Home', () => {
-    it('renders h1 element', () => {
-        render(<Home />)
-        expect(screen.getByText('All Posts')).toBeInTheDocument();
-    })
-})
+Enzyme.configure({ adapter: new Adapter() });
+
+import Home from "../Home";
+import { expect } from "chai";
+
+describe("enzyme-adapter-react-18", () => {
+  it("should initialize", () => {
+    expect(5).to.equal(5);
+  });
+});
